78afc9633b958737d2a0d58178cab5741c4f195c
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-12-11 22:57:57 +0200

    feat: add a simple cli interface

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
-public   class Main {
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
       /* 
         Stack stack = new Stack(5);
@@ -17,5 +18,62 @@ String expresstion3="{[()";
 
 System.out.println("Expresstion "+expresstion1 +"->"+(isBalanced(expresstion1)?"Balanced":"Not Balanced"));
 */
+      Scanner sc = new Scanner(System.in);
+      boolean run = true;
+
+      System.out.println("Please enter the stack size");
+      Stack s = new Stack(80);
+
+      while (run) {
+        System.out.println("\n=========== Choose an option ===========\n");
+        System.out.println("A) Re-nitialize the stack");
+        System.out.println("B) Push to the stack");
+        System.out.println("C) Pop from the stack");
+        System.out.println("D) Peek at the top element");
+        System.out.println("E) Check an expression");
+        System.out.println("F) Exit");
+
+        String input = sc.nextLine();
+        input = input.toUpperCase();
+        switch(input) {
+          case "A":
+            System.out.print("Please enter the stack size: ");
+            int size = sc.nextInt();
+            s = new Stack(size);
+            break;
+          case "B":
+            System.out.print("Please enter the element you want to push: ");
+            char element = sc.next().charAt(0);
+            s.push(element);
+            break;
+          case "C":
+            char c = s.pop();
+            if (s.isEmpty()) {
+              System.out.println("The stack is empty");
+              break;
+            }
+            System.out.print(c + " has been popped");
+            break;
+          case "D":
+            if (s.isEmpty()) {
+              System.out.println("The stack is empty");
+              break;
+            }
+            char top = s.peek();
+            System.out.print(top + " is at the top of the stack");
+            break;
+          case "E":
+            System.err.print("Enter the expression to check: ");
+            String expresstion = sc.nextLine();
+            System.out.println("Expresstion "+ expresstion +" is "+(BalancedParenthesisChecker.isBalanced(expresstion)?"Balanced":"Not Balanced"));
+            break;
+          case "F":
+            run = false;
+            break;
+          default:
+            System.out.println("Unknown command");
+            break;
+        }
+      }
     }
 }
