568523ebac5e5d850375de04d6a810f540a16c42
Mohamed Elfeky <97516177+Mohammedelfeky702@users.noreply.github.com>
2024-12-14 23:44:16 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-      /* 
-        Stack stack = new Stack(5);
-        stack.push('A');
-        stack.push('B');
-        stack.push('C');
-        System.out.println("Top of stack: " + stack.peek());
-        stack.pop();
-        System.out.println("Popped element "+stack.peek());
-        System.out.println("Top of stack: " + stack.peek());
-
-
-String expresstion1="{[()]}";
-String expresstion2="{[(])]}";
-String expresstion3="{[()";
-
-System.out.println("Expresstion "+expresstion1 +"->"+(isBalanced(expresstion1)?"Balanced":"Not Balanced"));
-*/
+  
       Scanner sc = new Scanner(System.in);
       boolean run = true;
 
